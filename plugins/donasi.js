42ef6dea27dfe1b16eaee4eff2febe6a831bfe5f
Tegtopia12 <108807495+Tegtopia12@users.noreply.github.com>
2022-07-08 18:01:27 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
             .✵.𝗚𝗥𝗨𝗣 JB|TEGUH.✵.
 
-            https://chat.whatsapp.com/H1DTdAv9Mxk61i3GW9yVhz
+            https://chat.whatsapp.com/EaiVhSl6S8ZBb6S0wC2s5x
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
